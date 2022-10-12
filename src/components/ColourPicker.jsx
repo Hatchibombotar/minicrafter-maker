0271f068a7a437922ba725a10e41b7d79687164b
Hatchibombotar <72224591+Hatchibombotar@users.noreply.github.com>
2022-10-12 21:05:40 +0100

    Made ColourPicker input area bigger
    final change for issue #2

--- src/components/ColourPicker.jsx
+++ src/components/ColourPicker.jsx
@@ -4,7 +4,7 @@ import styles from '../App.module.css';
 export default function ColourPicker({ onInput }) {
 	let [colour, setColour] = createSignal([0, 0, 0])
 	const picker = <input
-		style={{ opacity: 0 }}
+		style={{ opacity: 0, height: "35px", width: "35px"}}
 		type="color"
 		name="color"
 		onInput={(e) => {
